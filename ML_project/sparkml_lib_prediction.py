dfcb29f6679e0a164645015adfef107c14e36dbe
Ehsanmohsin66 <ehsanmohsin66@gmail.com>
2023-03-28 09:34:42 +0100

    last commit

--- ML_project/sparkml_lib_prediction.py
+++ ML_project/sparkml_lib_prediction.py
@@ -1,19 +1,8 @@
 #import happybase
 from pyspark.sql import SparkSession
 import logging
-from pyspark.ml.classification import RandomForestClassifier
-from pyspark.ml.classification import MultilayerPerceptronClassifier
 from pyspark.ml.regression import RandomForestRegressionModel,RandomForestRegressor
-from pyspark.ml import Pipeline
 from pyspark.ml.linalg import Vectors
-from pyspark.sql import Row
-from pyspark.ml.linalg import Vectors
-import json
-from pyspark.sql.functions import date_format
-from pyspark.sql.functions import col
-from pyspark.sql.types import IntegerType, DoubleType, StructType, StructField, StringType
-import pprint
-
 def transData(data):
     return data.rdd.map(lambda r: [r[4], Vectors.dense(r[0:4])]). \
         toDF(['label', 'features'])
@@ -29,34 +18,13 @@ if __name__ == '__main__':
     spark = SparkSession.builder.appName(
         "Spark_ML_train_model").getOrCreate()  # ("local[*]", "FirstDemo")
     spark.sparkContext.setLogLevel("ERROR")
-
-    TOPIC = "windpowerproject"
-
-
-    #for testing local vs non local
-    BOOSTRAP_SERVER = "ip-172-31-13-101.eu-west-2.compute.internal:9092"
-    df = spark.read.format("kafka").option("kafka.bootstrap.servers", BOOSTRAP_SERVER).\
-    option("subscribe", TOPIC).option("startingOffsets", "earliest")\
-    .option("endingOffsets", "latest").load()
-
-    new_df = df.selectExpr("CAST(value AS STRING)")
-    i=0
-    cont=df.count()
+    query="Select * from windpredictionproject_jan23.weather_Api_data"
+    new_df = spark.sql(query)
     model_loaded = RandomForestRegressionModel().load("ml_model")
-    #connection = happybase.Connection('ip-172-31-3-80.eu-west-2.compute.internal', table_prefix='ml_windpowerpred')
-    #schema1 = StructType([StructField('feature1', StringType(), True),StructField('feature2', DoubleType(), True),StructField('feature3', DoubleType(), True),StructField('feature4', DoubleType(), True)])
+    testData=new_df.rdd.map(lambda r:[0,Vectors.dense(r[2:6])]).toDF(['label', 'features'])
+    pred=model_loaded.transform(testData)
+    pred.show()
 
-    for i in range(cont-20,cont):
-        str1=new_df.collect()[i][0]
-        json_data=json.loads(str1)
-        pred_data_inp_list = [(json_data["datetime"], json_data["tempt_c"],json_data["wa_c"], json_data["tempt_c"])]
-        pred_data_inp_df = spark.createDataFrame(data=pred_data_inp_list,schema=["feature1","feature2","feature3","feature4"])
-        pred_data_inp_1=pred_data_inp_df.withColumn("feature1", date_format(col("feature1"), "D"))\
-        .withColumn("feature1",col("feature1").cast(IntegerType()))
-        testData=pred_data_inp_1.rdd.map(lambda r:[0,Vectors.dense(r[0:4])]).toDF(['label', 'features'])
-        pred=model_loaded.transform(testData)
-        pred.show()
-        date_time=json_data["datetime"]
         #df_forhbase=spark.createDataFrame([[date_time, pred_power]], ["datetime", "active_power"])
         #df_forhbase.show()
 
